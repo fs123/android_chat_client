285e59dd8c3a8fa33f3eb2a61a25b0c5585ad18d
Flavio <fs@sadeghi.ch>
2016-04-25 17:47:27 +0200

    first commit

---
+++ app/src/main/java/ch/fhnw/ws6/chat/ChatClient.java
@@ -0,0 +1,15 @@
+package ch.fhnw.ws6.chat;
+
+public class ChatClient {
+
+    private String userName;
+
+    public boolean login(String userName) {
+        this.userName = userName;
+        return true;
+    }
+
+    public void sendMessage(String message) {
+
+    }
+}
